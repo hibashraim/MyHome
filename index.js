79100c78de96ef8c54aa8f85e934b53f9da10e57
hibashraim <hiba.shraim.25@gmail.com>
2024-01-27 15:04:10 +0200

    update

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import express  from 'express';
 import initApp from './src/modules/app.router.js';
 
 const app=express();
-const PORT=process.env.PORT || 4000;
+const PORT=process.env.PORT;
 initApp(app,express);
+
 
 app.listen(PORT,()=>{
 console.log(`server is runing ${PORT}`);
